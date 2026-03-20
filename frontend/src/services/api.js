3205b4a91a28cf46a249680f9d84252bc42d5d1e
danielhabtamu1994-dev <danielhabtamu1994@gmail.com>
2026-03-20 14:06:13 +0300

    Create api.js

---
+++ frontend/src/services/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || '/api',
+});
+
+API.interceptors.request.use(cfg => {
+  const token = localStorage.getItem('token');
+  if (token) cfg.headers.Authorization = `Bearer ${token}`;
+  return cfg;
+});
+
+API.interceptors.response.use(
+  r => r,
+  err => {
+    if (err.response?.status === 401) {
+      localStorage.clear();
+      window.location.href = '/login';
+    }
+    return Promise.reject(err);
+  }
+);
+
+export default API;
